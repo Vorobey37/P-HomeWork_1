65feff3cb75104374a0a4fa57d5d3b725dcf3329
Abvorobey <abvorobey@gmail.com>
2023-04-10 12:07:03 +0300

    Задача про билетик

--- task_6.py
+++ task_6.py
@@ -13,7 +13,8 @@
 """
 a=input("Введите номер билета: ")
 
-if int(a[0])+int(a[1])+int(a[2]) == int(a[3])+int(a[4])+int(a[5]):
-    print("yes")
-else: print("no")
-
+if len(a) == 6:
+    if int(a[0])+int(a[1])+int(a[2]) == int(a[3])+int(a[4])+int(a[5]):
+        print("yes")
+    else: print("no")
+else: print("Номер билета какой-то неправильный!")
